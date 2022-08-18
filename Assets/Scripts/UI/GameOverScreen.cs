e494779d45ddbbcdb53f9f635ae4fdf7a5a79c92
Roma <rlepesh@mail.ru>
2022-08-18 18:26:55 +0300

    Add UI screen Start/GameOver

---
+++ Assets/Scripts/UI/GameOverScreen.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameOverScreen : Screen
+{
+    public event UnityAction RestartButttonClick;
+    public override void Close()
+    {
+        CanvasGroup.alpha = 0;
+        Button.interactable = false;
+    }
+
+    public override void Open()
+    {
+        CanvasGroup.alpha = 1;
+        Button.interactable = true;
+    }
+
+    protected override void OnButtonClick()
+    {
+        RestartButttonClick?.Invoke();
+    }
+}
---
+++ Assets/Scripts/UI/StartScreen.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StartScreen : Screen
+{
+    public event UnityAction PlayButttonClick;
+    public override void Close()
+    {
+        CanvasGroup.alpha = 0;
+        Button.interactable = false;
+    }
+
+    public override void Open()
+    {
+        CanvasGroup.alpha = 1;
+        Button.interactable = true;
+    }
+
+    protected override void OnButtonClick()
+    {
+        PlayButttonClick?.Invoke();
+    }
+}
